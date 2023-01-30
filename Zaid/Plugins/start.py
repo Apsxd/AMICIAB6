852713f14ec2eb95c7143fad1752a3a017b58051
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-01-30 16:29:42 +0530

    Update start.py

--- Zaid/Plugins/start.py
+++ Zaid/Plugins/start.py
@@ -81,16 +81,16 @@ async def start(event):
        await event.reply(
              f"{hi} {event.sender.first_name}\n{line}\n{hj}\n{hj2}\n{hj3}\n{hj4}\n{hj5}\n{hj6}\n{hj7}\n{hj8}\n‣ /setlang {hj9}\n{line}\n{hj10}", 
              buttons=[
-        [Button.url("Add me to your group ➕", url="t.me/Zaid2_Robot?startgroup=true")],
-        [Button.url("Channel 📢", url="t.me/TheUpdatesChannel"), Button.url("Support 🌎", url="t.me/thesupportchat")],
-        [Button.inline("language 🌐", data=f"langs"), Button.inline("Help ⁉️", data="help_menu")]])
+        [Button.url("Add me to your group", url="t.me/elsaRobot?startgroup=true")],
+        [Button.url("Channel", url="t.me/DrewNotFound"), Button.url("Support 🌎", url="t.me/tubots")],
+        [Button.inline("language", data=f"langs"), Button.inline("Help ⁉️", data="help_menu")]])
     if event.is_group:
         IMSG = f"{random.choice(IMG)}"
         await event.client.send_file(event.chat_id,
              IMSG,
              caption="{} {}".format(hi, event.sender.first_name), 
              buttons=[
-         [Button.url("Add me to your group ➕", url="t.me/Zaid2_Robot?startgroup=true")]])
+         [Button.url("Add me to your group", url="t.me/elsarobot?startgroup=true")]])
 
 
 JSONDB = None
