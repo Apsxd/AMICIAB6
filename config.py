7f9fc48c03a66c44716cc2e16431c497996c4cda
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-01-30 16:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "6435225"))
 API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
-DB_URI = os.environ.get("ELEPHANT_SQL", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-BOT_ID = int(os.environ.get("BOT_ID", ""))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1821123783:AAGLi7Hn7jup06uXJE9vVOpw6RwBhTmlEKg")
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Mongo_Db_Bot:@cluster0.hct06fi.mongodb.net/?retryWrites=true&w=majorityo")
+DB_URI = os.environ.get("ELEPHANT_SQL", "postgres://mcclbjwx:CqMrbec47cqL5KbaZOUDlVQWOscjNcKR@peanut.db.elephantsql.com/mcclbjwx")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1137799257"))
+BOT_ID = int(os.environ.get("BOT_ID", "1821123783"))
